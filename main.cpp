e7ba5816de0fe800379980e6bdf8c62e03ee7e8a
Ismael Mendes <ismaelmendes.isma@gmail.com>
2020-05-07 21:06:00 -0300

    Leitura dos Dados

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string sexo;
+    int cerveja, refri, espeto;
+
+    cout << "Sexo: ";
+    cin >> sexo;
+    cout << "Quantidade de cervejas:";
+    cin >> cerveja;
+    cout << "Quantidade de refrigerantes: ";
+    cin >> refri;
+    cout << "Quantidade de espetinhos: ";
+    cin >> espeto;
+
+
     return 0;
 }
